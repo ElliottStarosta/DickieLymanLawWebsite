5ef9707a1245b95922be6b7a6ece45aa9f477f21
elliottstarosta <starlliott@gmail.com>
2024-08-23 19:53:11 -0400

    updated timeout

--- js/main.js
+++ js/main.js
@@ -31,7 +31,7 @@ window.onload = function () {
     setTimeout(() => {
         toggle.className = 'nav__toggle';
         console.log("removed");
-    }, 300);
+    }, 125);
 };
 
 // Initialize the showMenu function
