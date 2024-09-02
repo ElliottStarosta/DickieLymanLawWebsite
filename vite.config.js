b234bbf36afae34629f1777f715dbf76a656a27a
elliottstarosta <starlliott@gmail.com>
2024-09-01 23:39:25 -0400

    updated vite.config.js for legal-disclaimer

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
 
 export default defineConfig({
-    build: {
-        rollupOptions: {
-            input: {
-                index: resolve(__dirname, 'public/index.html'),
-                legal: resolve(__dirname, 'public/legal-disclaimer.html')
-            }
-        }
+  build: {
+    rollupOptions: {
+      input: {
+        index: 'index.html',
+        legal: 'legal-disclaimer.html'
+      }
     }
+  }
 });
