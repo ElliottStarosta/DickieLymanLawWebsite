6b24cea0641aa96974f8e4e3455196f2efc74a60
Elliott <starlliott@gmail.com>
2024-12-31 14:43:52 -0500

    changed img path

--- js/main.js
+++ js/main.js
@@ -228,7 +228,7 @@ function changeTextOnResize() {
             logoElement = document.createElement('img');
             logoElement.setAttribute('data-aos', 'fade-down');
             logoElement.id = 'hero-text-logo';
-            logoElement.src = '/assets/logo-dl7fomzz.webp';
+            logoElement.src = '/assets/hero/webp/logo.webp';
             logoElement.alt = 'Dickie & Lyman Logo';
             heroTextContent.insertBefore(logoElement, pElement);
             logoElement.style.width = '450px';
